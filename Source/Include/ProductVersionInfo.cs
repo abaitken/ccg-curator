f40a5fa15134a058c19b6b699806a6557d82cc6f
Alex Boyne-Aitken <abaitken@live.co.uk>
2019-01-21 11:49:17 +0000

    Initial commit with basic framework projects

---
+++ Source/Include/ProductVersionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alex Boyne-Aitken")]
+[assembly: AssemblyProduct("CCG Curator")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
